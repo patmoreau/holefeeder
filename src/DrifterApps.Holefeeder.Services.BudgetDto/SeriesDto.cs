2c84de5009e7f8962ba61338559dcc2009da9e3f
Patrick Moreau <patrick@drifterapps.com>
2020-04-21 22:51:51 -0400

    Renaming Api to BudgetApi
    Renaming to better reflect the intent of the api

---
+++ src/DrifterApps.Holefeeder.Services.BudgetDto/SeriesDto.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DrifterApps.Holefeeder.Services.BudgetDto
+{
+    public class SeriesDto
+    {
+        public DateTime From { get; }
+        public DateTime To { get; }
+        public int Count { get; }
+        public decimal Amount { get; }
+
+        public SeriesDto(DateTime from, DateTime to, int count, decimal amount)
+        {
+            From = from;
+            To = to;
+            Count = count;
+            Amount = amount;
+        }
+    }
+}
