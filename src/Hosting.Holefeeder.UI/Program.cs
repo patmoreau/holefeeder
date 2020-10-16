a4c2d24ac1a922ae7d69d75584d816436bc1f0b5
Patrick Moreau <patrick@drifterapps.com>
2020-10-15 22:35:42 -0400

    refactoring

--- src/Hosting.Holefeeder.UI/Program.cs
+++ src/Hosting.Holefeeder.UI/Program.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace Hosting.UI
+namespace DrifterApps.Holefeeder.Hosting.Holefeeder.UI
 {
     public class Program
     {
