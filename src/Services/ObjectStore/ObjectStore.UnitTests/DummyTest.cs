fd8fb5dea7219323e2f4e23e8d89a70b554b6c34
Patrick Moreau <patrick@drifterapps.com>
2020-12-30 00:04:20 -0500

    adding failing test to make sure CI catches it

---
+++ src/Services/ObjectStore/ObjectStore.UnitTests/DummyTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace ObjectStore.UnitTests
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void DummyFailure()
+        {
+            true.Should().BeFalse();
+        }
+    }
+}
