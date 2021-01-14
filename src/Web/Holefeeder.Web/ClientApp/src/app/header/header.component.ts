1a91633116c7a7fa53cfc6a422b0e8f166ad13c0
Patrick Moreau <patrick@drifterapps.com>
2021-01-13 22:23:49 -0500

    migrating from implicit flow to code flow (#32)

--- src/Web/Holefeeder.Web/ClientApp/src/app/header/header.component.ts
+++ src/Web/Holefeeder.Web/ClientApp/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { DateService } from '@app/singletons/services/date.service';
 import { addDays, startOfToday } from 'date-fns';
@@ -12,11 +12,25 @@ import {
     faAngleDown
 } from '@fortawesome/free-solid-svg-icons';
 import { faCalendarCheck, faCalendarMinus, faCalendarPlus } from '@fortawesome/free-regular-svg-icons';
-import { BroadcastService, MsalService } from '@azure/msal-angular';
-import { Account } from 'msal';
-import { Subscription } from 'rxjs';
+import {Subject} from 'rxjs';
 import { SettingsService } from "@app/singletons/services/settings.service";
+import {MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalService} from "@azure/msal-angular";
+import {
+  AuthenticationResult, AuthError,
+  EventMessage,
+  EventType,
+  InteractionType,
+  PopupRequest,
+  RedirectRequest
+} from "@azure/msal-browser";
+import {b2cPolicies} from "@app/config/config.service";
+import {filter, takeUntil} from "rxjs/operators";
 
+interface IdTokenClaims extends AuthenticationResult {
+  idTokenClaims: {
+    acr?: string
+  }
+}
 
 @Component({
     selector: 'dfta-header',
@@ -45,25 +59,74 @@ export class HeaderComponent implements OnInit, OnDestroy {
     faCalendarCheck = faCalendarCheck;
     faCalendarPlus = faCalendarPlus;
 
+    isIframe = false;
     loggedIn = false;
-
-    subscriptions = new Array<Subscription>();
+    private readonly _destroying$ = new Subject<void>();
 
     constructor(
         private modalService: NgbModal,
         private dateService: DateService,
-        private authService: MsalService,
         private settingsService: SettingsService,
-        private broadcastService: BroadcastService,
         private router: Router,
+        @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
+        private authService: MsalService,
+        private msalBroadcastService: MsalBroadcastService
     ) {
         this.isNavbarCollapsed = true;
     }
 
     ngOnInit() {
+      this.isIframe = window !== window.parent && !window.opener;
 
-        this.profile = this.authService.getAccount();
-        this.loggedIn = !!this.profile;
+      this.checkAccount();
+
+      this.msalBroadcastService.msalSubject$
+        .pipe(
+          filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS || msg.eventType === EventType.ACQUIRE_TOKEN_SUCCESS),
+          takeUntil(this._destroying$)
+        )
+        .subscribe((result: EventMessage) => {
+
+          let payload: IdTokenClaims = <AuthenticationResult>result.payload;
+
+          // We need to reject id tokens that were not issued with the default sign-in policy.
+          // "acr" claim in the token tells us what policy is used (NOTE: for new policies (v2.0), use "tfp" instead of "acr")
+          // To learn more about b2c tokens, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview
+
+          if (payload.idTokenClaims?.acr === b2cPolicies.names.forgotPassword) {
+            window.alert('Password has been reset successfully. \nPlease sign-in with your new password.');
+            return this.authService.logout();
+          } else if (payload.idTokenClaims['acr'] === b2cPolicies.names.editProfile) {
+            window.alert('Profile has been updated successfully. \nPlease sign-in again.');
+            return this.authService.logout();
+          }
+
+          this.checkAccount();
+          return result;
+        });
+
+      this.msalBroadcastService.msalSubject$
+        .pipe(
+          filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_FAILURE || msg.eventType === EventType.ACQUIRE_TOKEN_FAILURE),
+          takeUntil(this._destroying$)
+        )
+        .subscribe((result: EventMessage) => {
+          if (result.error instanceof AuthError) {
+            // Check for forgot password error
+            // Learn more about AAD error codes at https://docs.microsoft.com/azure/active-directory/develop/reference-aadsts-error-codes
+            if (result.error.message.includes('AADB2C90118')) {
+
+              // login request with reset authority
+              let resetPasswordFlowRequest = {
+                scopes: ["openid"],
+                authority: b2cPolicies.authorities.forgotPassword.authority,
+              }
+
+              this.login(resetPasswordFlowRequest);
+            }
+          }
+        });
+
         if (this.loggedIn) {
             this.settingsService.loadUserSettings();
         }
@@ -71,22 +134,53 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.dateService.period.subscribe(period => {
             this.period = period;
         });
-
-        this.subscriptions.push(this.broadcastService.subscribe('msal:loginSuccess', (success) => {
-            this.profile = this.authService.getAccount();
-            this.loggedIn = !!this.profile;
-            if (this.loggedIn) {
-                this.settingsService.loadUserSettings();
-            }
-        }));
     }
 
     ngOnDestroy(): void {
-        this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+      this._destroying$.next(undefined);
+      this._destroying$.complete();
+    }
+
+    checkAccount() {
+      this.loggedIn = this.authService.instance.getAllAccounts().length > 0;
+    }
+
+    login(userFlowRequest?: RedirectRequest | PopupRequest) {
+      this.msalGuardConfig
+      if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
+        if (this.msalGuardConfig.authRequest) {
+          this.authService.loginPopup({...this.msalGuardConfig.authRequest, ...userFlowRequest} as PopupRequest)
+            .subscribe((response: AuthenticationResult) => {
+              this.authService.instance.setActiveAccount(response.account);
+              this.checkAccount();
+            });
+        } else {
+          this.authService.loginPopup(userFlowRequest)
+            .subscribe((response: AuthenticationResult) => {
+              this.authService.instance.setActiveAccount(response.account);
+              this.checkAccount();
+            });
+        }
+      } else {
+        if (this.msalGuardConfig.authRequest){
+          this.authService.loginRedirect({...this.msalGuardConfig.authRequest, ...userFlowRequest} as RedirectRequest);
+        } else {
+          this.authService.loginRedirect(userFlowRequest);
+        }
+      }
     }
 
     logout() {
-        this.authService.logout();
+      this.authService.logout();
+    }
+
+    editProfile() {
+      let editProfileFlowRequest = {
+        scopes: ["openid"],
+        authority: b2cPolicies.authorities.editProfile.authority,
+      }
+
+      this.login(editProfileFlowRequest);
     }
 
     open(content) {
