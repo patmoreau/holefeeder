1f86785fd8efc34523fb984ec3053cb3dd175b87
Patrick Moreau <patrick@drifterapps.com>
2022-02-10 00:19:59 -0500

    fixes

--- src/Web/Holefeeder.Web/Pages/Error.cshtml.cs
+++ src/Web/Holefeeder.Web/Pages/Error.cshtml.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Holefeeder.Web3.Pages;
+namespace Holefeeder.Web.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 public class ErrorModel : PageModel
