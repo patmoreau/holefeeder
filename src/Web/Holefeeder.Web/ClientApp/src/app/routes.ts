faabb2d9f2c656fde98e0ef3b0598b898e7abcca
Patrick Moreau <patrick@drifterapps.com>
2022-09-19 23:28:27 -0400

    cleanup

--- src/Web/Holefeeder.Web/ClientApp/src/app/routes.ts
+++ src/Web/Holefeeder.Web/ClientApp/src/app/routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
-import { ErrorHandler } from '@angular/core';
-import { GlobalErrorHandler } from '@app/core/errors/global-error-handler';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpLoadingInterceptor } from '@app/core/errors/http-loading.interceptor';
-import { environment } from '@env/environment';
 
 export const ROUTES: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./modules/home/routes').then(m => m.HOME_ROUTES),
-    providers: [],
   },
 ];
