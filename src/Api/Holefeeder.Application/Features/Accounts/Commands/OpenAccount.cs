178cc667b19de304514ba53060c144e829834bff
Patrick Moreau <patrick@drifterapps.com>
2022-12-11 23:00:45 -0500

    OpenAccount

--- src/Api/Holefeeder.Application/Features/Accounts/Commands/OpenAccount.cs
+++ src/Api/Holefeeder.Application/Features/Accounts/Commands/OpenAccount.cs
@@ -1,10 +1,12 @@
-﻿using Holefeeder.Application.Features.Accounts.Queries.GetAccount;
+﻿using Holefeeder.Application.Context;
+using Holefeeder.Application.Features.Accounts.Queries.GetAccount;
 using Holefeeder.Application.SeedWork;
 using Holefeeder.Domain.Features.Accounts;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 
 namespace Holefeeder.Application.Features.Accounts.Commands;
 
@@ -27,54 +29,43 @@ public class OpenAccount : ICarterModule
     }
 
     internal record Request(AccountType Type, string Name, DateTime OpenDate, decimal OpenBalance, string Description)
-        : IRequest<Guid>;
+        : ICommandRequest<Guid>;
 
     internal class Validator : AbstractValidator<Request>
     {
-        public Validator(IUserContext userContext, IAccountRepository repository)
+        public Validator()
         {
             RuleFor(command => command.Type).NotNull();
-            RuleFor(command => command.Name)
-                .NotNull()
-                .NotEmpty()
-                .Length(1, 255)
-                .MustAsync(async (name, cancellation) =>
-                    await repository.FindByNameAsync(name, userContext.UserId, cancellation) is null)
-                .WithMessage(x => $"Name '{x.Name}' already exists.")
-                .WithErrorCode("AlreadyExistsValidator");
+            RuleFor(command => command.Name).NotNull().NotEmpty().Length(1, 255);
             RuleFor(command => command.OpenDate).NotEmpty();
         }
     }
 
     internal class Handler : IRequestHandler<Request, Guid>
     {
-        private readonly IAccountRepository _repository;
         private readonly IUserContext _userContext;
+        private readonly BudgetingContext _context;
 
-        public Handler(IUserContext userContext, IAccountRepository repository)
+        public Handler(IUserContext userContext, BudgetingContext context)
         {
             _userContext = userContext;
-            _repository = repository;
+            _context = context;
         }
 
         public async Task<Guid> Handle(Request request, CancellationToken cancellationToken)
         {
-            try
+            if (await _context.Accounts.AnyAsync(x => x.Name == request.Name && x.UserId == _userContext.UserId,
+                    cancellationToken))
             {
-                var account = Account.Create(request.Type, request.Name, request.OpenBalance, request.OpenDate,
-                    request.Description, _userContext.UserId);
-
-                await _repository.SaveAsync(account, cancellationToken);
-
-                await _repository.UnitOfWork.CommitAsync(cancellationToken);
-
-                return account.Id;
+                throw new AccountDomainException($"Name '{request.Name}' already exists.");
             }
-            catch (AccountDomainException)
-            {
-                _repository.UnitOfWork.Dispose();
-                throw;
-            }
+
+            var account = Account.Create(request.Type, request.Name, request.OpenBalance, request.OpenDate,
+                request.Description, _userContext.UserId);
+
+            await _context.Accounts.AddAsync(account, cancellationToken);
+
+            return account.Id;
         }
     }
 }
---
+++ src/Api/Holefeeder.Tests.Common/Builders/Accounts/OpenAccountRequestBuilder.cs
@@ -0,0 +1,33 @@
+using static Holefeeder.Application.Features.Accounts.Commands.OpenAccount;
+
+namespace Holefeeder.Tests.Common.Builders.Accounts;
+
+internal class OpenAccountRequestBuilder : IBuilder<Request>
+{
+    private readonly Faker<Request> _faker = new AutoFaker<Request>()
+        .RuleFor(x => x.OpenDate, faker => faker.Date.Past().Date)
+        .RuleFor(x => x.OpenBalance, faker => faker.Finance.Amount());
+
+    public Request Build()
+    {
+        _faker.AssertConfigurationIsValid();
+        return _faker.Generate();
+    }
+
+    public OpenAccountRequestBuilder WithName(string name)
+    {
+        _faker.RuleFor(x => x.Name, name);
+        return this;
+    }
+
+    public OpenAccountRequestBuilder WithNoName()
+    {
+        _faker.RuleFor(x => x.Name, string.Empty);
+        return this;
+    }
+
+    public static OpenAccountRequestBuilder GivenAnOpenAccountRequest() => new();
+
+    public static OpenAccountRequestBuilder GivenAnInvalidOpenAccountRequest() =>
+        new OpenAccountRequestBuilder().WithNoName();
+}
