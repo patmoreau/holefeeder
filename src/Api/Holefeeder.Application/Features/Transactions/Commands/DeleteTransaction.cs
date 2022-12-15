ff4b13b3d4931be2315f11d815519e42481d43f6
Patrick Moreau <patrick@drifterapps.com>
2022-12-14 23:06:46 -0500

    DeleteTransaction

--- src/Api/Holefeeder.Application/Features/Transactions/Commands/DeleteTransaction.cs
+++ src/Api/Holefeeder.Application/Features/Transactions/Commands/DeleteTransaction.cs
@@ -1,10 +1,11 @@
-﻿using Holefeeder.Application.Features.Transactions.Exceptions;
+﻿using Holefeeder.Application.Context;
+using Holefeeder.Application.Features.Transactions.Exceptions;
 using Holefeeder.Application.SeedWork;
-using Holefeeder.Domain.Features.Transactions;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 
 namespace Holefeeder.Application.Features.Transactions.Commands;
 
@@ -26,7 +27,7 @@ public class DeleteTransaction : ICarterModule
             .RequireAuthorization();
     }
 
-    internal record Request(Guid Id) : IRequest<Unit>;
+    internal record Request(Guid Id) : ICommandRequest<Unit>;
 
     internal class Validator : AbstractValidator<Request>
     {
@@ -38,27 +39,26 @@ public class DeleteTransaction : ICarterModule
 
     internal class Handler : IRequestHandler<Request, Unit>
     {
-        private readonly ITransactionRepository _transactionRepository;
         private readonly IUserContext _userContext;
+        private readonly BudgetingContext _context;
 
-        public Handler(IUserContext userContext, ITransactionRepository transactionRepository)
+        public Handler(IUserContext userContext, BudgetingContext context)
         {
             _userContext = userContext;
-            _transactionRepository = transactionRepository;
+            _context = context;
         }
 
         public async Task<Unit> Handle(Request request, CancellationToken cancellationToken)
         {
             var transaction =
-                await _transactionRepository.FindByIdAsync(request.Id, _userContext.UserId, cancellationToken);
+                await _context.Transactions.SingleOrDefaultAsync(
+                    x => x.Id == request.Id && x.UserId == _userContext.UserId, cancellationToken);
             if (transaction is null)
             {
                 throw new TransactionNotFoundException(request.Id);
             }
 
-            await _transactionRepository.DeleteAsync(request.Id, _userContext.UserId, cancellationToken);
-
-            await _transactionRepository.UnitOfWork.CommitAsync(cancellationToken);
+            _context.Remove(transaction);
 
             return Unit.Value;
         }
