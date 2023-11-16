edb7f2dc0e4e53dc11fd2e0bab6dade8faef62e2
Patrick Moreau <patrick@drifterapps.com>
2023-11-16 15:59:43 -0500

    Add PWA

--- src/Web/Holefeeder.Web/ClientApp/src/app/app.component.ts
+++ src/Web/Holefeeder.Web/ClientApp/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { AuthActions } from '@app/core/store/auth/auth.actions';
 import { Store } from '@ngrx/store';
 import { AppStore, CategoriesActions } from '@app/core/store';
 import { AuthFeature } from '@app/core/store/auth/auth.feature';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,29 @@ import { AuthFeature } from '@app/core/store/auth/auth.feature';
   imports: [CommonModule, RouterModule],
 })
 export class AppComponent implements OnInit {
+  promptEvent: any;
+  isAppInstalled: boolean = false;
+
   private store = inject(Store<AppStore>);
+  private swUpdate = inject(SwUpdate);
 
   ngOnInit() {
+    // Check if the app is installed
+    window.addEventListener('beforeinstallprompt', event => {
+      event.preventDefault();
+      this.promptEvent = event;
+      this.isAppInstalled = true;
+    });
+
+    // Check for updates
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(event => {
+        if (confirm('A new version is available. Load it?')) {
+          window.location.reload();
+        }
+      });
+    }
+
     this.store.dispatch(AuthActions.checkAuth());
 
     this.store
@@ -27,5 +48,13 @@ export class AppComponent implements OnInit {
           this.store.dispatch(CategoriesActions.clearCategories());
         }
       });
+  }
+
+  addToHomeScreen() {
+    if (this.promptEvent) {
+      this.promptEvent.prompt();
+    } else {
+      console.log('App is already installed.');
+    }
   }
 }
--- src/Web/Holefeeder.Web/ClientApp/src/main.ts
+++ src/Web/Holefeeder.Web/ClientApp/src/main.ts
@@ -24,6 +24,7 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { appEffects, appStore } from '@app/core/store';
 import { provideEffects } from '@ngrx/effects';
 import { HttpRequestLoggerInterceptor } from '@app/core/interceptors/http-request-logger.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.production) {
   enableProdMode();
@@ -34,69 +35,67 @@ bootstrapApplication(AppComponent, {
     { provide: 'BASE_API_URL', useValue: `${environment.baseUrl}/gateway` },
     loadConfigProvider,
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
-    importProvidersFrom(
-      RouterModule.forRoot(ROUTES, {
+    importProvidersFrom(RouterModule.forRoot(ROUTES, {
         enableTracing: !environment.production && environment.enableTracing,
-      }),
-      AuthModule.forRoot({
+    }), AuthModule.forRoot({
         config: {
-          authority:
-            'https://holefeeder.b2clogin.com/holefeeder.onmicrosoft.com/B2C_1A_signup_signin_drifterapps/v2.0',
-          authWellknownEndpointUrl:
-            'https://holefeeder.b2clogin.com/holefeeder.onmicrosoft.com/B2C_1A_signup_signin_drifterapps/v2.0/.well-known/openid-configuration',
-          redirectUrl: window.location.origin,
-          postLogoutRedirectUri: window.location.origin,
-          clientId: '9814ecda-b8db-4775-a361-714af29fe486',
-          scope:
-            'openid profile offline_access https://holefeeder.onmicrosoft.com/holefeeder.api/holefeeder.user',
-          responseType: 'code',
-          silentRenew: true,
-          useRefreshToken: true,
-          ignoreNonceAfterRefresh: true,
-          maxIdTokenIatOffsetAllowedInSeconds: 600,
-          issValidationOff: false, // this needs to be true if using a common endpoint in Azure
-          autoUserInfo: false,
-          logLevel: LogLevel.Error,
-          customParamsAuthRequest: {
-            prompt: 'select_account', // login, consent
-          },
-          secureRoutes: [
-            'https://holefeeder.localtest.me',
-            'http://localhost:3000',
-            '/gateway/api',
-          ],
+            authority: 'https://holefeeder.b2clogin.com/holefeeder.onmicrosoft.com/B2C_1A_signup_signin_drifterapps/v2.0',
+            authWellknownEndpointUrl: 'https://holefeeder.b2clogin.com/holefeeder.onmicrosoft.com/B2C_1A_signup_signin_drifterapps/v2.0/.well-known/openid-configuration',
+            redirectUrl: window.location.origin,
+            postLogoutRedirectUri: window.location.origin,
+            clientId: '9814ecda-b8db-4775-a361-714af29fe486',
+            scope: 'openid profile offline_access https://holefeeder.onmicrosoft.com/holefeeder.api/holefeeder.user',
+            responseType: 'code',
+            silentRenew: true,
+            useRefreshToken: true,
+            ignoreNonceAfterRefresh: true,
+            maxIdTokenIatOffsetAllowedInSeconds: 600,
+            issValidationOff: false,
+            autoUserInfo: false,
+            logLevel: LogLevel.Error,
+            customParamsAuthRequest: {
+                prompt: 'select_account', // login, consent
+            },
+            secureRoutes: [
+                'https://holefeeder.localtest.me',
+                'http://localhost:3000',
+                '/gateway/api',
+            ],
         },
-      })
-    ),
+    })),
     provideStore(appStore),
     provideEffects(appEffects),
     provideStoreDevtools({
-      maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+        maxAge: 25,
+        logOnly: !isDevMode(),
+        autoPause: true,
+        trace: false,
+        traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
     }),
     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpLoadingInterceptor,
-      multi: true,
+        provide: HTTP_INTERCEPTORS,
+        useClass: HttpLoadingInterceptor,
+        multi: true,
     },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: JsonDateOnlyInterceptor,
-      multi: true,
+        provide: HTTP_INTERCEPTORS,
+        useClass: JsonDateOnlyInterceptor,
+        multi: true,
     },
     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpRequestLoggerInterceptor,
-      multi: true,
+        provide: HTTP_INTERCEPTORS,
+        useClass: HttpRequestLoggerInterceptor,
+        multi: true,
     },
     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpLoadingInterceptor,
-      multi: true,
+        provide: HTTP_INTERCEPTORS,
+        useClass: HttpLoadingInterceptor,
+        multi: true,
     },
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 }).catch(err => console.error(err));
