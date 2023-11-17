c59d1833e2a0f6c8f1e7cb71dbee1a2671d1a8a9
Patrick Moreau <patrick@drifterapps.com>
2023-11-16 19:26:08 -0500

    fix for ios pwa

--- src/Web/Holefeeder.Web/ClientApp/src/app/app.component.ts
+++ src/Web/Holefeeder.Web/ClientApp/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
+import { Platform } from '@angular/cdk/platform';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthActions } from '@app/core/store/auth/auth.actions';
 import { Store } from '@ngrx/store';
 import { AppStore, CategoriesActions } from '@app/core/store';
 import { AuthFeature } from '@app/core/store/auth/auth.feature';
-import { SwUpdate } from '@angular/service-worker';
+import {
+  SwUpdate,
+  VersionEvent,
+  VersionReadyEvent,
+} from '@angular/service-worker';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,27 +20,36 @@ import { SwUpdate } from '@angular/service-worker';
   imports: [CommonModule, RouterModule],
 })
 export class AppComponent implements OnInit {
-  promptEvent: any;
-  isAppInstalled: boolean = false;
+  isOnline = false;
+  modalVersion = false;
+  modalPwaEvent: any;
+  modalPwaPlatform: string | undefined;
 
   private store = inject(Store<AppStore>);
   private swUpdate = inject(SwUpdate);
+  private platform = inject(Platform);
 
   ngOnInit() {
-    // Check if the app is installed
-    window.addEventListener('beforeinstallprompt', event => {
-      event.preventDefault();
-      this.promptEvent = event;
-      this.isAppInstalled = true;
-    });
+    this.updateOnlineStatus();
 
-    // Check for updates
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+
     if (this.swUpdate.isEnabled) {
-      this.swUpdate.available.subscribe(event => {
-        if (confirm('A new version is available. Load it?')) {
-          window.location.reload();
-        }
-      });
+      this.swUpdate.versionUpdates.pipe(
+        filter(
+          (evt: VersionEvent): evt is VersionReadyEvent =>
+            evt.type === 'VERSION_READY'
+        ),
+        map((evt: VersionReadyEvent) => {
+          console.info(
+            `currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`
+          );
+          this.modalVersion = true;
+        })
+      );
+
+      this.loadModalPwa();
     }
 
     this.store.dispatch(AuthActions.checkAuth());
@@ -50,11 +65,45 @@ export class AppComponent implements OnInit {
       });
   }
 
-  addToHomeScreen() {
-    if (this.promptEvent) {
-      this.promptEvent.prompt();
-    } else {
-      console.log('App is already installed.');
+  private updateOnlineStatus(): void {
+    this.isOnline = window.navigator.onLine;
+    console.info(`isOnline=[${this.isOnline}]`);
+  }
+
+  public updateVersion(): void {
+    this.modalVersion = false;
+    window.location.reload();
+  }
+
+  public closeVersion(): void {
+    this.modalVersion = false;
+  }
+
+  private loadModalPwa(): void {
+    if (this.platform.ANDROID) {
+      window.addEventListener('beforeinstallprompt', (event: any) => {
+        event.preventDefault();
+        this.modalPwaEvent = event;
+        this.modalPwaPlatform = 'ANDROID';
+      });
     }
+
+    if (this.platform.IOS && this.platform.SAFARI) {
+      const isInStandaloneMode =
+        'standalone' in window.navigator &&
+        (<any>window.navigator)['standalone'];
+      if (!isInStandaloneMode) {
+        this.modalPwaPlatform = 'IOS';
+      }
+    }
+  }
+
+  public addToHomeScreen(): void {
+    this.modalPwaEvent.prompt();
+    this.modalPwaPlatform = undefined;
+  }
+
+  public closePwa(): void {
+    this.modalPwaPlatform = undefined;
   }
 }
