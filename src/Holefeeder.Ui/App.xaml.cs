8e5b9300e1d87b26a3abb41c00b7a397b0a0c8b5
Patrick Moreau <patrick@drifterapps.com>
2024-11-01 00:25:14 -0400

    switch to maui new project

---
+++ src/Holefeeder.Ui/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Holefeeder.Ui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+
+    public static void PerformActionForShortcutItem(string shortcutItemType)
+    {
+        if (shortcutItemType == "com.drifterapps.Holefeeder.purchase")
+        {
+            NavigateToPurchase();
+        }
+    }
+
+    private static void NavigateToPurchase() => Ui.MainPage.NavigateToPurchase();
+}
