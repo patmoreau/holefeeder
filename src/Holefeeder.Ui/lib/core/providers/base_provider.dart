7d6b48e177d242ce2e1ec7ffb3d21b8a5ca339bc
Patrick Moreau <patrick@drifterapps.com>
2025-04-10 13:25:24 -0400

    Remove flutter ui

--- src/Holefeeder.Ui/lib/core/providers/base_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:holefeeder/core/utils/rest_client.dart';
-
-abstract class BaseProvider {
-  RestClient restClient;
-
-  BaseProvider({required this.restClient});
-
-  void dispose() {}
-}
--- src/Holefeeder.Ui/lib/core/utils/extensions.dart
+++
@@ -1,5 +0,0 @@
-extension ListExtensions<T> on List<Map<String, dynamic>> {
-  List<T> fromJsonList(T Function(Map<String, dynamic>) fromJson) {
-    return map((json) => fromJson(json)).toList();
-  }
-}
